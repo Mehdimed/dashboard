aaae798933bf5921e101ccdd1b18dee65df6ff7c
Mehdi Agounine <agounine.mehdi@gmail.com>
2022-11-02 17:26:42 +0100

    add teamPage with pagination

--- src/routes/Team.jsx
+++ src/routes/Team.jsx
@@ -1,37 +1,102 @@
 import { useEffect, useState } from "react";
 
 export default function Team() {
-
   const [team, setTeam] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [links, setLinks] = useState({});
 
   useEffect(() => {
-    fetch('http://localhost:3000/team')
-      .then(res => res.json())
-      .then(data => {
-        setTimeout(() => {
-          setTeam(data);
-        setLoading(false);
-        }, 2000);
-      })
+    fetchTeam("http://localhost:3000/team?_page=1&_limit=13");
   }, []);
 
-  
+  const fetchTeam = async (url) => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => {
+        setLinks(extractLinks(res.headers.get("link")));
+        return res.json();
+      })
+      .then((data) => {
+        setTeam(data);
+        setLoading(false);
+      });
+  };
+
+
+  function extractLinks(links) {
+    let res = {};
+
+    links.split(", ").map((link) => {
+      if (link.includes('rel="next"')) {
+        res.next = link.split("; ")[0].slice(1, -1);
+      } else if (link.includes('rel="prev"')) {
+        res.prev = link.split("; ")[0].slice(1, -1);
+      } else if (link.includes('rel="first"')) {
+        res.first = link.split("; ")[0].slice(1, -1);
+      } else if (link.includes('rel="last"')) {
+        res.last = link.split("; ")[0].slice(1, -1);
+      }
+    });
+
+    return res;
+  }
 
   return (
-    <>
-      <h1 className="text-2xl font-bold mt-3 ml-3">Team</h1>
+    <div className="pl-12 pr-12 pt-6">
+      <h1 className="text-2xl font-bold">Team</h1>
       <span className="text-sm">gestion des membres de l'équipe</span>
-      {loading && <h1>Loading...</h1>}
-
-      {!loading && team.map(member => (
-        <div key={member.id}>
-          <h1>{member.name}</h1>
-          <h2>{member.title}</h2>
-          <p>{member.bio}</p>
+      
+      <div className=" flex flex-col gap-2 p-3 mt-6 rounded border-solid border-gray-500 border-2">
+        
+        <div className="flex flex-row font-bold">
+          <div className="flex flex-1">Nom</div>
+          <div className="flex flex-1">Email</div>
+          <div className="flex flex-1 justify-center">Age</div>
+          <div className="flex flex-1">Tél</div>
+          <div className="flex flex-1">Role</div>
         </div>
-      ))}
 
-    </>
-  )
+        {loading && <h1>Loading...</h1>}
+
+        {!loading &&
+          team.map((member) => (
+            <div className="flex flex-row border-t-2 border-solid border-gray-500" key={member.id}>
+              <div className="flex flex-1">{member.name}</div>
+              <div className="flex flex-1">{member.email}</div>
+              <div className="flex flex-1 justify-center">{member.age}</div>
+              <div className="flex flex-1">{member.phone}</div>
+              <div className="flex flex-1">{member.access}</div>
+            </div>
+          ))}
+
+      </div>
+
+      <div className="absolute bottom-4 right-32 flex flex-row justify-center gap-2">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+          onClick={() => fetchTeam(links.first)}
+        >
+          &lt;&lt;
+        </button>
+        <button
+          className={`text-white py-2 px-4 rounded ${links.prev ? "bg-blue-500 hover:bg-blue-700" :" bg-gray-500 cursor-default"}`}
+          onClick={() => fetchTeam(links.prev)}
+        >
+          précédent
+        </button>
+        <button
+          className={`text-white py-2 px-4 rounded ${links.next ? "bg-blue-500 hover:bg-blue-700" :" bg-gray-500 cursor-default"}`}
+          onClick={() => fetchTeam(links.next)}
+        >
+          suivant
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+          onClick={() => fetchTeam(links.last)}
+        >
+          &gt;&gt;
+        </button>
+      </div>
+    </div>
+  );
 }
